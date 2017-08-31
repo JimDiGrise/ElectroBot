7f7b6303a434dd7e56144182a1accbfdab649d01
reutov <reutov.ilya@gmail.com>
2017-08-31 19:16:38 +0300

    Save gif to server

--- src/core/Gif.php
+++ src/core/Gif.php
@@ -18,7 +18,25 @@
             $this->logger = new Logger('Gif');
             $this->logger->pushHandler(new StreamHandler(__DIR__ . '/../../log/main.log', Logger::INFO));
         }
-       
+        public function createGif() 
+        {   
+            $gc = new GifCreator();
+            $duration = array_fill(0, count($this->images), 40);
+            $gc->create($this->images, $duration); 
+            $this->logger->info("Create gif");          
+            return $this->saveGif($gc->getGif());
+            
+        }
+        private function saveGif($binary) 
+        {
+            try {
+                if(empty($this->gifPath)) throw new Exception("Path is empty");
+                if(empty($binary)) throw new Exception("Binary is empty");
+                return file_put_contents($this->gifPath, $binary) ? TRUE : FALSE;
+            } catch(Exception $e) {
+                $this->logger->info("Exeption: " . $e->getMessage());
+            }
+        }
      
     }
     
