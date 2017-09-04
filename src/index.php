448535fd702a78c04a344476cc65b19279f0e37b
reutov <reutov.ilya@gmail.com>
2017-09-04 18:28:52 +0300

    Add main cycle

---
+++ src/index.php
@@ -0,0 +1,19 @@
+<?php 
+    require "../vendor/autoload.php";
+
+    
+    require "core/Bot.php";
+    require "core/Commands.php";
+    
+    $bot = new Bot("https://api.telegram.org/", "bot310341855:AAGF60Bu1mHjDjjEn31ekxwJmKw-OMTBlqg/");
+    while(1) {
+        sleep(3);
+        $cmd = new Commands($bot);
+        if(empty($bot->getLastMessage()) == FALSE) {
+            $lastMessage = $bot->getLastMessage();
+            $cmd->setLastChatId($bot->lastChatId);
+            $bot->confirmMessage($bot->lastChatId);
+           $cmd->handleCommand($lastMessage->message->text);
+        }
+    }
+?>
